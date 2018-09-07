106b7a8a449a6e8889d1f5071a2a9692ed46ac35
msalcedosuarez <35785975+msalcedosuarez@users.noreply.github.com>
2018-09-07 00:20:23 -0700

    Changes

--- BinaryIterativeSearch.java
+++ BinaryIterativeSearch.java
@@ -4,18 +4,26 @@
 public class BinaryIterativeSearch implements Practice2Search{
 
 	public int search(int[] a, int target){
+
+		int lower = 0;
+		int upper = a.length - 1;
+		int mid = 0;
+
+
 		while(lower <= upper){
-			//int mid = 
-		}
-		if(a[mid] == target){
-			//do something
-		}
-		else{
-			//adjust upper//lower
-		}
+			mid = (lower + last) / 2; 
 
+			if(a[mid] < target){
+				lower = mid + 1;
+			}
+			else if(a[mid] > target){
+				upper = mid - 1;
+			}
+			else{
+				return mid;
+			}
+		}
 		return -1;
-
 	}
 
 	public String searchName(){
@@ -24,3 +32,22 @@ public class BinaryIterativeSearch implements Practice2Search{
 	}
 
 }
+
+
+	// int lower = 0;
+	// 	int upper = a.length - 1;
+	// 	int mid = 0;
+
+
+	// 	while(lower <= upper){
+	// 		mid = (lower + last) / 2; 
+
+	// 		if(a[mid] == target){
+	// 			//do something
+	// 		}
+	// 		else{
+	// 			//adjust upper//lower
+	// 		}
+	// 	}
+
+	// 	return -1;
